1e8193e7a8d1a52445b52a2ca3aaf7be892a3994
binoy <brdalal@uh.edu>
2019-05-01 23:26:12 -0500

    added support for doc2vec and refactored code

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import GenericUnivariateSelect, chi2
 from sklearn.neural_network import MLPClassifier
 from datetime import datetime
-from feature_extraction import word_ngrams, char_ngrams, pos_ngrams
+
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+
+from feature_extraction import word_ngrams, char_ngrams, pos_ngrams, doc2vec
 
 data = pd.read_pickle('data/scaled_features.pkl')
 
@@ -17,28 +21,12 @@ data_new = data.copy()
 data_new = data_new.drop(data_new[data_new.Gender == 'M'].sample(frac=1 - frac).index)
 data_new['Blog'] = data_new['Blog'].values.astype(str)
 
-data_train_val, data_test, gender_train_val, gender_test = train_test_split(
-    data_new.loc[:, data_new.columns != 'Gender'], data_new.Gender, test_size=0.1, shuffle=True,
-    stratify=data_new.Gender, random_state=42)
-
-data_train, data_val, gender_train, gender_val = train_test_split(data_train_val, gender_train_val, shuffle=True,
-                                                                  test_size=0.22, stratify=gender_train_val,
-                                                                  random_state=42)
-data_folds = np.array_split(data_train_val, 10)
-gender_folds = np.array_split(gender_train_val, 10)
-
 ngram_range = (2, 8)
 binary = True
 count = 50000
-scores = []
-for i in range(10):
-    start_time = datetime.now()
-    test_data = data_folds[i]
-    test_gender = gender_folds[i]
 
-    train_data = pd.concat(data for data in data_folds[:i] + data_folds[i + 1:])
-    train_gender = pd.concat(gender for gender in gender_folds[:i] + gender_folds[i + 1:])
 
+def get_features(train_data, test_data, train_gender):
     cv_word, transformed_words_train = word_ngrams(train_data, ngram_range=ngram_range, binary=binary)
     cv_char, transformed_char_train = char_ngrams(train_data, ngram_range=ngram_range, binary=binary)
     cv_pos, transformed_pos_train = pos_ngrams(train_data, ngram_range=ngram_range, binary=binary)
@@ -54,38 +42,83 @@ for i in range(10):
     selector = GenericUnivariateSelect(chi2, 'k_best', param=count)
     selected_train = selector.fit_transform(transformed_train, train_gender)
     selected_test = selector.transform(transformed_test)
+    return selected_train, selected_test
 
-    clf = MLPClassifier(hidden_layer_sizes=(75, 75), solver='adam', activation='identity', early_stopping=True,
-                        max_iter=2500, random_state=42)
-    clf.fit(selected_train, train_gender)
-    score = clf.score(selected_test, test_gender)
-    scores.append(score)
-    print("Score for iteration ", i, " = ", score)
+
+def cross_val(clf, data_folds, gender_folds, feature_gen=True):
+    scores = []
+    for i in range(10):
+        start_time = datetime.now()
+        test_data = data_folds[i]
+        test_gender = gender_folds[i]
+
+        if feature_gen:
+            train_data = pd.concat(data for data in data_folds[:i] + data_folds[i + 1:])
+            train_gender = pd.concat(gender for gender in gender_folds[:i] + gender_folds[i + 1:])
+            selected_train, selected_test = train_data, test_data
+        else:
+            train_data = np.concatenate(tuple([data for data in data_folds[:i] + data_folds[i + 1:]]))
+            train_gender = np.concatenate(tuple([gender for gender in gender_folds[:i] + gender_folds[i + 1:]]))
+            selected_train, selected_test = train_data, test_data
+
+        clf.fit(selected_train, train_gender)
+        score = clf.score(selected_test, test_gender)
+        scores.append(score)
+        print("Score for iteration ", i, " = ", score)
+        print("Time taken = ", datetime.now() - start_time)
+
+    print("\n10-fold CV scores = ", scores)
+    print("Mean CV score = ", np.mean(scores))
+
+
+def test_set(clf, data_train, data_test, gender_train, gender_test, feature_gen=True):
+    start_time = datetime.now()
+    if feature_gen:
+        selected_train, selected_test = get_features(data_train, data_test, gender_train)
+    else:
+        selected_train, selected_test = data_train, data_test
+
+    clf.fit(selected_train, gender_train)
+    print("\nScore on test data = ", clf.score(selected_test, gender_test))
     print("Time taken = ", datetime.now() - start_time)
-    print("Params = ", clf.get_params())
 
-print("10-fold CV scores = ", scores)
-print("Mean CV score = ", np.mean(scores))
 
-cv_word, transformed_words_train = word_ngrams(data_train, ngram_range=ngram_range, binary=binary)
-cv_char, transformed_char_train = char_ngrams(data_train, ngram_range=ngram_range, binary=binary)
-cv_pos, transformed_pos_train = pos_ngrams(data_train, ngram_range=ngram_range, binary=binary)
-data_train = data_train.drop(columns=['Blog', 'POS'])
-transformed_words_test = cv_word.transform(data_test.Blog)
-transformed_char_test = cv_char.transform(data_test.Blog)
-transformed_pos_test = cv_pos.transform(data_test.POS)
-data_test = data_test.drop(columns=['Blog', 'POS'])
+# data_train_val, data_test, gender_train_val, gender_test = train_test_split(
+#     data_new.loc[:, data_new.columns != 'Gender'], data_new.Gender, test_size=0.1, shuffle=True,
+#     stratify=data_new.Gender, random_state=42)
+#
+# data_train, data_val, gender_train, gender_val = train_test_split(data_train_val, gender_train_val, shuffle=True,
+#                                                                   test_size=0.22, stratify=gender_train_val,
+#                                                                   random_state=42)
 
-transformed_train = hstack((transformed_words_train, transformed_char_train, transformed_pos_train, data_train),
-                           format='csr')
-transformed_test = hstack((transformed_words_test, transformed_char_test, transformed_pos_test, data_test),
-                          format='csr')
-selector = GenericUnivariateSelect(chi2, 'k_best', param=count)
-selected_train = selector.fit_transform(transformed_train, gender_train)
-selected_test = selector.transform(transformed_test)
+# data_folds = np.array_split(data_train_val, 10)
+# gender_folds = np.array_split(gender_train_val, 10)
+#
+# # MLP
+# clf = MLPClassifier(hidden_layer_sizes=(75, 75), solver='adam', activation='identity', early_stopping=True,
+#                     max_iter=2500, random_state=42)
+# cross_val(clf, data_folds, gender_folds)
+# test_set(clf, data_train, data_test)
+#
+# # XGBoost
+# clf = XGBClassifier(n_jobs=-1, max_depth=35, n_estimators=200, booster='dart', importance_type='total_gain')
+# cross_val(clf, data_folds, gender_folds)
+# test_set(clf, data_train, data_test)
+
+# Doc2Vec
+data_train_val, data_test = train_test_split(data_new[['Blog', 'Gender']], shuffle=True,
+                                             test_size=0.1, stratify=data_new['Gender'],
+                                             random_state=42)
+
+data_train, data_test, gender_train, gender_test = doc2vec(data_train_val, data_test)
+data_folds = np.array_split(data_train, 10)
+gender_folds = np.array_split(gender_train, 10)
 
 clf = MLPClassifier(hidden_layer_sizes=(75, 75), solver='adam', activation='identity', early_stopping=True,
                     max_iter=2500, random_state=42)
-clf.fit(selected_train, gender_train)
-print("Score on test data = ", clf.score(selected_test, gender_test))
-print("Params = ", clf.get_params())
+cross_val(clf, data_folds, gender_folds, False)
+test_set(clf, data_train, data_test, gender_train, gender_test, False)
+
+clf = SVC(C=2500, gamma='auto')
+cross_val(clf, data_folds, gender_folds, False)
+test_set(clf, data_train, data_test, gender_train, gender_test, False)
